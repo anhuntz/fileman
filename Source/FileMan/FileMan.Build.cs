20167c2e89e21fe014d13aa6731bd86ad4c5865e
Aaron Hunt <anhuntz@gmail.com>
2020-10-19 14:32:41 -0400

    Cleanup

--- Source/FileMan/FileMan.Build.cs
+++ Source/FileMan/FileMan.Build.cs
@@ -1,5 +1,3 @@
-// Some copyright should be here...
-
 using UnrealBuildTool;
 
 public class FileMan : ModuleRules
@@ -10,14 +8,12 @@ public class FileMan : ModuleRules
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
-				// ... add public include paths required here ...
 			}
 			);
 				
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
-				// ... add other private include paths required here ...
 			}
 			);
 			
@@ -26,7 +22,6 @@ public class FileMan : ModuleRules
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
 			}
 			);
 			
@@ -38,7 +33,6 @@ public class FileMan : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
 			}
 			);
 		
@@ -46,7 +40,6 @@ public class FileMan : ModuleRules
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
-				// ... add any modules that your module loads dynamically here ...
 			}
 			);
 	}
